0bacd4443a007e45106bb870a5487c6844774a40
Abdillahi485 <daakircadde143@gmail.com>
2024-10-07 03:11:56 +0300

    headerfooter done

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,27 @@
+import {Nav,Navbar,Container} from 'react-bootstrap';
+import {FaShoppingCart, FaUser} from 'react-icons/fa';
+import logo from '../assets/logo.png';
+const Header = () => {
+  return (
+    <header>
+        <Navbar bg='dark' variant='dark' expand='md' collapseOnSelect>   
+            <Container >
+                <Navbar.Brand href='/'>
+                <img src={logo} alt="proshop" />
+                Proshop 
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls='basic-navbar-nav' />
+                <Navbar.Collapse id='basic-navbar-nav'>
+                    <Nav className='ms-auto'>
+                        <Nav.Link href='/cart'><FaShoppingCart/> Cart</Nav.Link>
+                        <Nav.Link href='/loging'><FaUser/>Sign In</Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+
+    </header>
+  )
+}
+
+export default Header
